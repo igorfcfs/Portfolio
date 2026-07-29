418cb77caba30b3cad0639bc04ef6a81bb8dd478
Igor Fernando Casita Ferreira da Silva <79226308+igorfcfs@users.noreply.github.com>
2026-07-28 22:16:01 -0300

    Updates

---
+++ 522-e53468ded6ff38b4e1ab.js
@@ -0,0 +1,1 @@
+(self.webpackChunkv4=self.webpackChunkv4||[]).push([[522],{38522:function(){}}]);
--- sw.js
+++ sw.js
@@ -27,124 +27,18 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3ed46e890aee6dff90fe.js"
+    "url": "webpack-runtime-292c87140f8bc732239e.js"
   },
   {
-    "url": "framework-249d733f289d0429e609.js"
-  },
-  {
-    "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-909185ac39e0e18fce56.js"
+    "url": "framework-ab410140ee7e83874641.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "633466a84b4eafa0998d61b8a9ce4dd5"
-  },
-  {
-    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9a5d1562692cb053f3c3.js"
-  },
-  {
-    "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "97678572661eb7279c37baa9c9c83e5d"
-  },
-  {
-    "url": "page-data/app-data.json",
-    "revision": "3cf3fa3d8f63a43f5a31b3261770f693"
-  },
-  {
-    "url": "polyfill-1bb1d5bc2a1b3b1c1ed4.js"
-  },
-  {
-    "url": "d9067523-bc2e2120bc5afd67012e.js"
-  },
-  {
-    "url": "cb355538-178bc024ee0354330138.js"
-  },
-  {
-    "url": "546641dfde76ed00139ea2c3eae09499c5f2164a-9f4535481cc24d106a2c.js"
-  },
-  {
-    "url": "component---src-pages-404-js-8edb19dd75b2ebef2628.js"
-  },
-  {
-    "url": "page-data/en/404/page-data.json",
-    "revision": "ae23f15339058bb83872ba8fa09daa7b"
-  },
-  {
-    "url": "page-data/sq/d/1127135046.json",
-    "revision": "f0770305c6257fc95163816ecc5b0a6c"
-  },
-  {
-    "url": "page-data/sq/d/1921109229.json",
-    "revision": "2933d12244fa496d1962a5cc1d8c3744"
-  },
-  {
-    "url": "page-data/sq/d/1994492073.json",
-    "revision": "934957b4c39aa60521eebc4c84d0671c"
-  },
-  {
-    "url": "page-data/sq/d/2596216473.json",
-    "revision": "02514eca5a96ee9d8119a8c0c6075bbd"
-  },
-  {
-    "url": "page-data/sq/d/3208555609.json",
-    "revision": "c284503a8513d5546cc4bec617d6b7d8"
-  },
-  {
-    "url": "page-data/sq/d/3825832676.json",
-    "revision": "f7a4463258ee8d201699931c3b24f8ab"
-  },
-  {
-    "url": "page-data/sq/d/3933733069.json",
-    "revision": "99af142ebfbbf85a5a77d1338c0ee1df"
-  },
-  {
-    "url": "component---src-pages-archive-js-c1a0326558dd2cf6deed.js"
-  },
-  {
-    "url": "page-data/en/archive/page-data.json",
-    "revision": "8473e467367181676e3c8ce227b6b7db"
-  },
-  {
-    "url": "component---src-pages-index-js-cc48a2e0b5889cca5e1f.js"
-  },
-  {
-    "url": "page-data/en/page-data.json",
-    "revision": "585e7ce0b1f220ad5517a4fd92c09af5"
-  },
-  {
-    "url": "page-data/en/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "e111a626fabb0a0a81e76b11a5f04694"
-  },
-  {
-    "url": "component---src-pages-pensieve-index-js-f4f58be33cab975a8867.js"
-  },
-  {
-    "url": "page-data/en/pensieve/page-data.json",
-    "revision": "de9908e27068d519ddfeefc1968236fe"
-  },
-  {
-    "url": "page-data/pt/404/page-data.json",
-    "revision": "b4e25fb0a252f14146f8884d07be3fdc"
-  },
-  {
-    "url": "page-data/pt/archive/page-data.json",
-    "revision": "995e6457e078378c0ccd86f115788c4d"
-  },
-  {
-    "url": "page-data/pt/page-data.json",
-    "revision": "a23b442d55fe99bce67321c6b72fe1ca"
-  },
-  {
-    "url": "page-data/pt/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "8bca855046a2e987c26a36d769b7fc25"
-  },
-  {
-    "url": "page-data/pt/pensieve/page-data.json",
-    "revision": "43d6c4e69cfe3a86d82ee90a9804e16d"
+    "revision": "638324bcb9aa559165b1d2d568933d59"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "0592274b12dc5bd3f1dfc92f8a2287c9"
+    "revision": "58d4a5241cad7a120ef75e72eb2f918b"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -170,6 +64,24 @@ const MessageAPI = {
 
   clearPathResources: event => {
     event.waitUntil(idbKeyval.clear())
+
+    // We detected compilation hash mismatch
+    // we should clear runtime cache as data
+    // files might be out of sync and we should
+    // do fresh fetches for them
+    event.waitUntil(
+      caches.keys().then(function (keyList) {
+        return Promise.all(
+          keyList.map(function (key) {
+            if (key && key.includes(`runtime`)) {
+              return caches.delete(key)
+            }
+
+            return Promise.resolve()
+          })
+        )
+      })
+    )
   },
 
   enableOfflineShell: () => {
@@ -236,7 +148,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/Portfolio/app-9d0a3f8353c544bb7f9e.js`))) {
+  if (!resources || !(await caches.match(`/Portfolio/app-9f06800f4d31950a01be.js`))) {
     return await fetch(event.request)
   }
 
